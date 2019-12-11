1383a7e1c8c6c89c1fecc47367b98c8ca013cac0
Sameer Subba <sameer.libang@gmail.com>
2019-12-11 11:11:35 +1100

    new event form validation with materialize style

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,24 +4,27 @@ class EventsController < ApplicationController
   end
 
   def create
-    @event = current_user.events.build(
-      event_params.merge(
-        start_time: start_date_time,
-        end_time: end_date_time
-      )
-    )
+#    @event = current_user.events.build(
+#      event_params.merge(
+#        start_time: start_date_time,
+#        end_time: end_date_time
+#      )
+#    )
+
+    @event = current_user.events.build(event_params)
 
     if @event.save
       flash[:success] = 'Event saved'
     else
       flash[:error] = 'Something went wrong'
-      render :new
     end
   end
 
   private
 
   def start_date_time
+    return if params[:start_date].empty? || params[:start_time].empty?
+
     start_date = Date.parse(params[:start_date])
     start_time = Time.parse(params[:start_time])
 
@@ -29,6 +32,8 @@ class EventsController < ApplicationController
   end
 
   def end_date_time
+    return if params[:end_date].empty? || params[:end_time].empty?
+
     end_date = Date.parse(params[:end_date])
     end_time = Time.parse(params[:end_time])
     (end_date + end_time.seconds_since_midnight.seconds).to_datetime
@@ -36,7 +41,11 @@ class EventsController < ApplicationController
 
   def event_params
     params.require(:event).permit(
-      :title
+      :title,
+      :event_start_date,
+      :event_start_time,
+      :event_end_date,
+      :event_end_time
     )
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,24 @@
 class Event < ApplicationRecord
+  attr_accessor :event_start_date, :event_end_date, :event_start_time, :event_end_time
+
   belongs_to :user
+
+  validates_presence_of :title,
+                        :event_start_date,
+                        :event_start_time,
+                        :event_end_date,
+                        :event_end_time
+
+  #after_save :save_date_time
+
+  private
+
+  def save_date_time
+    self.start_time = start_date_time
+  end
+
+  def start_date_time
+    start_date = Date.parse(event_start_date)
+    start_time = Time.parse(event_start_time)
+  end
 end
