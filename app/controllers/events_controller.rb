395f2be531590fb859b6b5f44ec7e50a9db260b1
Sameer Subba <sameer.libang@gmail.com>
2019-12-19 11:10:53 +1100

    event create update with shared form errors display

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,13 +1,31 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: [:update]
+
   def index
     events = current_user.events
     @presented_events = events.map do |event|
-      EventPresenter.new(event)
+      event_presenter(event)
+    end
+  end
+
+  def edit 
+    event = current_user.events.find_by_id(params[:id])
+    event_presenter(event)
+  end 
+
+  def update
+    respond_to do |format|
+      if @event.update(event_params)
+        flash.now[:notice] = 'Event updated successfully'
+      else
+        format.js {render partial: 'form_errors'}
+      end
     end
   end
 
   def new
-    @event = Event.new
+    event = Event.new
+    event_presenter(event)
   end
 
   def create
@@ -17,13 +35,21 @@ class EventsController < ApplicationController
       if @event.save
         redirect_to events_path
         flash[:notice] = 'Event saved'
+      else
+        format.js { render partial: 'form_errors' }
       end
-
-      format.js
     end
   end
 
   private
+
+  def event_presenter(event)
+    @presented_event = EventPresenter.new(event)
+  end
+
+  def set_event
+    @event = Event.find_by_id(params[:id])
+  end
 
   def event_params
     params.require(:event).permit(
--- app/presenters/event_presenter.rb
+++ app/presenters/event_presenter.rb
@@ -3,7 +3,6 @@ class EventPresenter
 
   delegate_missing_to :event
 
-
   def initialize(event)
     @event = event
   end
@@ -14,5 +13,21 @@ class EventPresenter
 
   def end_time
     event.end_time.strftime("%c")
+  end
+
+  def event_start_date
+    event.start_time&.strftime('%b %d, %Y')
+  end
+
+  def event_end_date
+    event.end_time&.strftime('%b %d, %Y')
+  end
+
+  def event_start_time
+    event.start_time&.strftime('%I:%M %p' )
+  end
+
+  def event_end_time
+    event.end_time&.strftime('%I:%M %p' )
   end
 end
