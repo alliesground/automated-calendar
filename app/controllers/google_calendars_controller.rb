25df55926c82add24c9fa9c30226133dfaaf6129
Sameer Subba <sameer.libang@gmail.com>
2019-12-26 12:05:15 +1100

    creating remote calendar

--- app/controllers/google_calendars_controller.rb
+++ app/controllers/google_calendars_controller.rb
@@ -1,17 +1,52 @@
 class GoogleCalendarsController < ApplicationController
-  def index
-    client = Signet::OAuth2::Client.new(client_options)
-    client.update!(current_user.google_calendar_config.authorization)
 
-    service = Google::Apis::CalendarV3::CalendarService.new
+  def index
+    get_service do |service|
+      @calendar_list = service.list_calendar_lists
+    end
+  end
+
+  def new
+    @google_calendar = GoogleCalendar.new
+  end
+
+  def create
+    calendar = Google::Apis::CalendarV3::Calendar.new(
+      summary: params[:google_calendar][:name]
+    )
+
+    get_service do |service|
+      response = service.insert_calendar(calendar)
+    end
+  end
+
+  private
+
+  def get_service
+    client.update!(current_user.google_calendar_config.authorization)
     service.authorization = client
 
     begin
-      @calendar_list = service.list_calendar_lists
+      yield service
     rescue Google::Apis::AuthorizationError
       response = client.refresh!
       current_user.google_calendar_config.authorization.merge(response)
       retry
     end
+  end
+
+  def google_calendar_params
+    params.require(:google_calendar).permit(
+      :name,
+      :description
+    )
+  end
+
+  def client
+    @client ||= Signet::OAuth2::Client.new(client_options)
+  end
+
+  def service
+    @service ||= Google::Apis::CalendarV3::CalendarService.new
   end
 end
