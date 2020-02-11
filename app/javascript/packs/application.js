3d0472b3141f3f71b01d8b38d9b8c70859464e2c
Sameer <sameer.libang@gmail.com>
2020-02-12 00:07:07 +1100

    moving outbound_event_config_form js to packs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("packs/init_form_materialize")
 require("packs/flash_message")
+require("packs/outbound_event_config_form")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
