30bac2b57ba7e81dbfaa495ddc9eb73d6f6984c1
Sameer <sameer.libang@gmail.com>
2020-03-07 10:21:36 +1100

    configuring precompile assets false

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,7 @@ module ContactManager
     config.generators.system_tests = nil
 
     config.autoload_paths << Rails.root.join('lib')
+
+    config.assets.initialize_on_precompile = false
   end
 end
